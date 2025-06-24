cfe89758c94641f87ac78837a4b9adb337b1eb71
ChandruJCM14 <chandrujcm92@gmail.com>
2025-06-24 09:12:34 +0530

    Delete week3/day1 directory

--- week3/day1/CanaraBank.java
+++
@@ -1,8 +0,0 @@
-package week3.day1;
-
-public abstract class CanaraBank implements Payments{
-	public void recordPaymentDetails() {
-		System.out.println("Payments will be made by below possibilities");
-	}
-
-}
--- week3/day1/DataBridge.java
+++
@@ -1,9 +0,0 @@
-package week3.day1;
-
-public abstract class DataBridge implements DatabaseConnection {
-	
-	void executeQuery()
-	{
-		System.out.println("The Application is Launched");
-	}
-}
